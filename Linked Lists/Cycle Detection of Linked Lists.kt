4d0dd92929d6919481b0f5bf045205edbb76d8bc
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2020-12-13 00:57:14 +0800

    Update Cycle Detection of Linked Lists.kt

--- Linked Lists/Cycle Detection of Linked Lists.kt
+++ Linked Lists/Cycle Detection of Linked Lists.kt
@@ -1,4 +1,4 @@
-        fun hasCycle(head: ListNode?): Boolean {
+    fun hasCycle(head: ListNode?): Boolean {
         var slow: ListNode? = head
         var fast: ListNode? = head?.next
         while(fast != null){
